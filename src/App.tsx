0b05e3273150872d64a009539010b3f0132b66a4
Ian Pinili <gianpinili@gmail.com>
2024-04-04 12:47:48 -0700

    set up routes, components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Nav from "./components/Nav"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contacts from "./components/Contacts"
 function App() {
 
   return (
     <>
-      <h1>Ian's Portfolio</h1>
+     <BrowserRouter>
+     <Nav />
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contacts" element={<Contacts />} />
+     </Routes>
+     </BrowserRouter>
+
     </>
   )
 }
