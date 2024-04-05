f0285b63d8b91c2feb015d8363def6b239839151
Ian Pinili <gianpinili@gmail.com>
2024-04-05 01:40:48 -0700

    add maintenance landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Nav from "./components/Nav"
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contacts from "./components/Contacts"
-import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-     <Nav />
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contacts" element={<Contacts />} />
-     </Routes>
-      <Footer />
-     </BrowserRouter>
-
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
